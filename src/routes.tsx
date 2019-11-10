e2b47affe34733fba563c63ff615c1bbc482cc53
Wellington Araujo <wellingtonaraujosoares@gmail.com>
2019-11-10 17:57:33 -0300

    Configured routes and installed types

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import { HOME } from "src/constants/routes";
+
+import Home from "src/pages/Home";
+
 const Routes: React.SFC = () => {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Route exact path={HOME}  component={Home}/> 
+    </BrowserRouter>
+  )
 }
 
 export default Routes;
